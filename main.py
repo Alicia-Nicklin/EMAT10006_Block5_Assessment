645a045c647a5efda35cdc3182dacc12609edb00
Alicia-Nicklin <alicia@nicklin.name>
2024-03-17 12:34:51 +0000

    Initial checkin

---
+++ main.py
@@ -0,0 +1,15 @@
+####################################################################
+#                                                                  #
+#          EMAT10006 Further Computer Programming  March 2024      #
+#                                                                  #
+#                 Block 5 Assessment Group 18 (Victoria)           #
+#                                                                  #
+#                  Archie Miller ?                                 #
+#                   Tommy Newman ?                                 #
+#                     Eric Ogden ?                                 #
+#                 Alicia Nicklin rp23953@bristol.ac.uk             #
+#                                                                  #
+####################################################################
+
+if __name__ == "__main__":
+ print("Hello World!")
